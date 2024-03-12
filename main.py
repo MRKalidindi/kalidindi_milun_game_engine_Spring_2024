591b6af125435ddf0d3392f0a1cc87e22c60d140
MRKalidindi <milunraju@gmail.com>
2024-03-12 12:13:10 -0700

    Commented Out Mob2 to fix bug

--- main.py
+++ main.py
@@ -26,27 +26,27 @@ from os import path
 
 # initialize class
 # cooldown class
-# class Cooldown():
-#     # sets all properties to zero when instantiated...
-#     def __init__(self):
-#         self.current_time = 0
-#         self.event_time = 0
-#         self.delta = 0
-#         # ticking ensures the timer is counting...
-#     # must use ticking to count up or down
-#     def ticking(self):
-#         self.current_time = float((pg.time.get_ticks())/1000)
-#         self.delta = self.current_time - self.event_time
-#     # resets event time to zero - cooldown reset
-#     def countdown(self, x):
-#         x = x - self.delta
-#         if x != None:
-#             return x
-#     def event_reset(self):
-#         self.event_time = floor((pg.time.get_ticks())/1000)
-#     # sets current time
-#     def timer(self):
-#         self.current_time = floor((pg.time.get_ticks())/1000)
+class Cooldown():
+    # sets all properties to zero when instantiated...
+    def __init__(self):
+        self.current_time = 0
+        self.event_time = 0
+        self.delta = 0
+        # ticking ensures the timer is counting...
+    # must use ticking to count up or down
+    def ticking(self):
+        self.current_time = float((pg.time.get_ticks())/1000)
+        self.delta = self.current_time - self.event_time
+    # resets event time to zero - cooldown reset
+    def countdown(self, x):
+        x = x - self.delta
+        if x != None:
+            return x
+    def event_reset(self):
+        self.event_time = floor((pg.time.get_ticks())/1000)
+    # sets current time
+    def timer(self):
+        self.current_time = floor((pg.time.get_ticks())/1000)
 
 # Game Class
 class Game:
@@ -111,7 +111,7 @@ class Game:
                     Coin(self, col, row) 
                 if tile == 'M':
                     Mob(self, col, row)
-                if tile == 'M':
+                if tile == '2':
                     Mob2(self, col, row)
 
 
--- sprites.py
+++ sprites.py
@@ -23,7 +23,7 @@ def collide_with_walls(sprite, group, dir):
             if hits[0].rect.centery < sprite.rect.centery:
                 sprite.pos.y = hits[0].rect.bottom + sprite.rect.height / 2
             sprite.vel.y = 0
-            sprite.rect.centery = sprite.pos.y  
+            sprite.rect.centery = sprite.pos.y 
 
 # write a player class
 class Player(pg.sprite.Sprite):
@@ -214,39 +214,39 @@ class Mob(pg.sprite.Sprite):
         self.rect.y = self.y
         self.collide_with_walls('y')
         
-class Mob2(pg.sprite.Sprite):
-    def __init__(self, game, x, y):
-        self.groups = game.all_sprites, game.mobs
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
-        # self.image = game.mob_img
-        self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image.fill(ORANGE)
-        self.rect = self.image.get_rect()
-        # self.hit_rect = MOB_HIT_RECT.copy()
-        # self.hit_rect.center = self.rect.center
-        self.pos = vec(x, y) * TILESIZE
-        self.vel = vec(0, 0)
-        self.acc = vec(0, 0)
-        self.rect.center = self.pos
-        self.rot = 0
-        # added
-        self.speed = 150
-        # self.health = MOB_HEALTH
+# class Mob2(pg.sprite.Sprite):
+#     def __init__(self, game, x, y):
+#         self.groups = game.all_sprites, game.mobs
+#         pg.sprite.Sprite.__init__(self, self.groups)
+#         self.game = game
+#         # self.image = game.mob_img
+#         self.image = pg.Surface((TILESIZE, TILESIZE))
+#         self.image.fill(ORANGE)
+#         # self.rect = self.image.get_rect()
+#         # self.hit_rect = MOB_HIT_RECT.copy()
+#         # self.hit_rect.center = self.rect.center
+#         self.pos = vec(x, y) * TILESIZE
+#         self.vel = vec(0, 0)
+#         self.acc = vec(0, 0)
+#         self.rect.center = self.pos
+#         self.rot = 0
+#         # added
+#         self.speed = 150
+#         # self.health = MOB_HEALTH
 
-    def update(self):
-        self.rot = (self.game.player.rect.center - self.pos).angle_to(vec(1, 0))
-        self.image = pg.transform.rotate(self.image, self.rot)
-        self.rect = self.image.get_rect()
-        self.rect.center = self.pos
-        self.acc = vec(self.speed, 0).rotate(-self.rot)
-        self.acc += self.vel * -1
-        self.vel += self.acc * self.game.dt
-        self.pos += self.vel * self.game.dt + 0.5 * self.acc * self.game.dt ** 2
-        # self.hit_rect.centerx = self.pos.x
-        collide_with_walls(self, self.game.walls, 'x')
-        # self.hit_rect.centery = self.pos.y
-        collide_with_walls(self, self.game.walls, 'y')
-        # self.rect.center = self.hit_rect.center
-        # if self.health <= 0:
-        #     self.kill()
+#     def update(self):
+#         self.rot = (self.game.player.rect.center - self.pos).angle_to(vec(1, 0))
+#         self.image = pg.transform.rotate(self.image, self.rot)
+#         self.rect = self.image.get_rect()
+#         self.rect.center = self.pos
+#         self.acc = vec(self.speed, 0).rotate(-self.rot)
+#         self.acc += self.vel * -1
+#         self.vel += self.acc * self.game.dt
+#         self.pos += self.vel * self.game.dt + 0.5 * self.acc * self.game.dt ** 2
+#         # self.hit_rect.centerx = self.pos.x
+#         collide_with_walls(self, self.game.walls, 'x')
+#         # self.hit_rect.centery = self.pos.y
+#         collide_with_walls(self, self.game.walls, 'y')
+#         # self.rect.center = self.hit_rect.center
+#         # if self.health <= 0:
+#         #     self.kill()
